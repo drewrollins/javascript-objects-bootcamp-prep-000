b82c36fc6b4ea72cc17b6b7f12d4715ada7013f9
Drew Rollins <drewrollins14@gmail.com>
2018-05-01 16:15:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({artist: "song"});
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
-  return playlist
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
